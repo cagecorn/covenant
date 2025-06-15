dd15b0ce3a2195a53565a67a1c4b41285077e90e
cagecorn <lmcbv@naver.com>
2025-06-16 05:25:50 +0900

    Add managers module

---
+++ js/managers.js
@@ -0,0 +1,9 @@
+// [매니저] 이 파일은 모든 매니저 클래스를 정의합니다.
+import { logManager } from './main.js';
+
+// 각 매니저 클래스를 'export' 합니다.
+export class BattleLogManager { /* ... 기존 BattleLogManager 클래스 코드 ... */ }
+export class VisualEffectManager { /* ... 기존 VisualEffectManager 클래스 코드 ... */ }
+export class EventManager { /* ... 기존 EventManager 클래스 코드 ... */ }
+export class StatusEffectManager { /* ... 기존 StatusEffectManager 클래스 코드 ... */ }
+export const battleMaster = { /* ... 기존 battleMaster 객체 코드 ... */ };
